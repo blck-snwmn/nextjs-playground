7d6e471fce0d38a20d2703224383a1688d81a433
blck-snwmn <whitesnowmancreator@gmail.com>
2021-08-04 03:30:05 +0900

    Show createdAt

--- pages/books/[id].tsx
+++ pages/books/[id].tsx
@@ -1,20 +1,31 @@
+import { useRouter } from "next/router";
+
 export async function getStaticPaths() {
   return {
     paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
-    fallback: false,
+    fallback: true,
   };
 }
 
 export async function getStaticProps({ params }) {
   return {
-    props: { book: { name: "test", id: params.id } },
+    props: { book: { name: "test", id: params.id, createdAt: Date.now() } },
   };
 }
 
 function Books({ book }) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  const now = new Date(book.createdAt);
+
   return (
     <div>
-      {book.name}:{book.id}
+      {book.id}-{book.name}:{now.toLocaleDateString()}-
+      {now.toLocaleTimeString()}
     </div>
   );
 }
